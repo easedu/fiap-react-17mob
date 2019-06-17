0bc43c31773e2a4e5f9ebb11caf403f05df11a84
easedu <edu.arruda.souza@gmail.com>
2019-06-17 19:30:27 -0300

    Ajuste tela de pesquisa

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import axios from 'axios'
-import { Redirect } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class Search extends Component {
-    constructor() {
+    constructor(){
         super();
 
         this.onSearch = this.onSearch.bind(this);
-    }
-
-    state = {
-        results:[],
-        redirect: false
+        this.state = {
+            results: [],
+        };
     }
 
     onSearch(event) {
@@ -20,35 +18,46 @@ class Search extends Component {
         axios.get(`https://api.mercadolibre.com/sites/MLB/search?q=${value}`)
             .then(({ data }) => {
                 this.setState({ results: data.results, redirect: true })
-                console.log(this.state.results);
             })
     }
 
-    renderRedirect = (id) => {
-        if (this.state.redirect) {
-            return <Redirect to={`/product/${id}`} />
-        }
+    renderIds(resultId){
+        return (
+            <div key={ resultId.id }>
+                <ul className="mdl-list">
+                    <li className="mdl-list__item mdl-list__item--two-line">
+                        <span className="mdl-list__item-primary-content">
+                            <span>{ resultId.title }</span>
+                        </span>
+                        <span className="mdl-list__item-secondary-content">
+                            <Link to={ `/product/${resultId.id}`}> 
+                                Abrir
+                            </Link>
+                        </span>
+                    </li>
+                </ul>    
+            </div>
+            
+        )
     }
 
     render() {
         const { results } = this.state;
-        console.log(this.state.results);
-        let resultId;
-        if (results[0]) {
-            console.log(results[0].id);
-            resultId = results[0].id;
-            console.log(resultId);
-        }
         return (
             <div>
-                <input type="text" onChange={this.onSearch} />
                 <div>
-                    {this.renderRedirect(resultId)}
-                    <button onClick={this.setRedirect}>Redirect</button>
+                    <div className="mdl-cell mdl-cell--12-col mdl-cell--N-col-phone">
+                        <div className="mdl-cell--middle">
+                            <input type="text" placeholder="Buscar" onChange={ this.onSearch } />
+                            <ul>
+                                { results.map(this.renderIds) }
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
-        )
+        );
     }
-};
+}
 
 export default Search;
